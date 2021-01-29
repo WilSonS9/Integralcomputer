0aaee2c295e6d7255bc111c4c2b6975aa61dd644
William Sonnerklev <54662723+WilSonS9@users.noreply.github.com>
2021-01-29 15:48:03 +0100

    Comments, cleaned up code

--- integral.py
+++ integral.py
@@ -5,23 +5,17 @@ from matplotlib.offsetbox import AnchoredText
 import numpy as np
 
 def f(x):
+    # Example function, enter your own to compute definite integrals for different functions
     return (x**4+4*x**3)
 
+# Divides the interval [a,b] into 2^N rectangles and calculates the total area under the curve
 def integera (N, a, b):
     rects = 2**N
     langd = b - a
-    #print('längd: ' + str(langd))
+
     baseForN = langd / rects
 
-    #print('bas: ' + str(baseForN))
     area = 0
-    # for c in range(0, rects):
-    #     d = -1 + (2 * (c + 1))
-    #     midp = (c + 1) * baseForN / 2
-    # #    print('midpoint = ' + str(midp))
-    #     vall = baseForN * (f(midp))
-    # #    print('vall= ' + str(vall))
-    #     area += vall
     
     t = np.arange(a, b, 0.01)
     s = f(t)
@@ -41,12 +35,10 @@ def integera (N, a, b):
         rec = patches.Rectangle((a + j * (baseForN), 0), baseForN, f(midp), edgecolor='black', facecolor='r', linewidth=1.5, alpha=0.25)
         ax.add_patch(rec)
         pass
-    # if area<0:
-    #     area *= -1
     texts = AnchoredText('Area = ' + str(round(area, 4)), loc=1)
     ax.add_artist(texts)
-  #  ax.text(0, 0, 'Area = ' + str(round(area, 4)), horizontalalignment='left', verticalalignment='top')
     fig.savefig("integral.png")
     plt.show()
     return area
+# Define your own values of N, a and b in order to compute the desired integral
 integera(14,5,10)
